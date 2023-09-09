70e64238282c5386ca31c2962a61cd671b76f366
dawittamene <dawittamene559@gmail.com>
2023-09-09 16:22:15 +0300

    home page

--- index.php
+++ index.php
@@ -2,217 +2,53 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>zumbara library</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-	<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <link rel="stylesheet" href="index.css">
+    <title>home page</title>
+    <!--from awesome cdn link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<!--coustom css file link-->
+<link rel="stylesheet" href="nav.css">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-	  <a class="navbar-brand"><span>zu</span>mbara</a>
-	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-	   
-	  </button>
-
-	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-	    <ul class="navbar-nav mr-auto">
-	    
-		  <li class="nav-item">
-		  </li>
-		  <li class="nav-item">
-			<a class="nav-link " href="#" ><span>H</span>ome</a>
-		  </li>
-
-		  <li class="nav-item">
-			<a class="nav-link " href="#" ><span>B</span>ook</a>
-		  </li>
-
-		  <li class="nav-item">
-			<a class="nav-link " href="#" ><span>v</span>ideo</a>
-		  </li>
-
-		  <li class="nav-item">
-			<a class="nav-link " href="#" ><span>A</span>bout</a>
-		  </li>
-	    </ul>
-
-      
-	  </div>
-	</nav>
-	<br><br>
-	<div class="row">
-		<div class="col-lg-2">
-        <img class="thumbnail" src="images/placeholder.png">
-			<div class="box-element product">
-				<h6><strong>Product</strong></h6>
-				<hr>
-				<center><a class="btn btn-outline-success" href="#"><button><i class="fa fa-download"></i> Download</button></a></center>
-
-			</div>
-		</div>
-
-		<div class="col-lg-2">
-        <img class="thumbnail" src="images/placeholder.png">
-			<div class="box-element product">
-				<h6><strong>Product</strong></h6>
-				<hr>
-				
-				<center><a class="btn btn-outline-success" href="#"><button><i class="fa fa-download"></i> Download</button></a></center>
-				
-				
-			</div>
-		</div>
-
-
-	
-
-
-		<div class="col-lg-2">
-        <img class="thumbnail" src="images/placeholder.png">
-			<div class="box-element product">
-				<h6><strong>Product</strong></h6>
-				<hr>
-				
-				<center><a class="btn btn-outline-success" href="#"><button><i class="fa fa-download"></i> Download</button></a></center>
-				
-				
-			</div>
-		</div>
-
-
-		<div class="col-lg-2">
-			<img class="thumbnail" src="images/placeholder.png">
-				<div class="box-element product">
-					<h6><strong>Product</strong></h6>
-					<hr>
-					
-					<center><a class="btn btn-outline-success" href="#"><button><i class="fa fa-download"></i> Download</button></a></center>
-					
-					
-				</div>
-			</div>
-
-
-			<div class="col-lg-2">
-				<img class="thumbnail" src="images/placeholder.png">
-					<div class="box-element product">
-						<h6><strong>Product</strong></h6>
-						<hr>
-						
-						<center><a class="btn btn-outline-success" href="#"><button><i class="fa fa-download"></i> Download</button></a></center>
-						
-						
-					</div>
-				</div>
-
-
-        <div class="col-lg-2">
-        <img class="thumbnail" src="images/placeholder.png">
-			<div class="box-element product">
-				<h6><strong>Product</strong></h6>
-				<hr>
-				
-				<center><a class="btn btn-outline-success" href="#"><button><i class="fa fa-download"></i> Download</button></a></center>
-				
-				
-				
-			</div>
-		</div>
+    <!--header section starts-->
 
-	
-	</div>
+<header>
+    <div id="menu-bar" class="fas fa-bars"></div>
+    <a href="home.php" class="logo"><span>z</span>umbara <span> L</span>ibrary</a>
+    <nav class="navbar">
+        <a href="index.php">home</a>
+        <a href="student.php">Academic Book</a>
+        <a href="#">Ethiopia history</a>
+        
+        
+        <a href="contact.php">contact</a>
+		<a href="login_form.php" id="log">Login</a>
+    </nav>
+    
+</header>
 
 
 
 
-	<div class="row">
-		<div class="col-lg-2">
-        <img class="thumbnail" src="images/placeholder.png">
-			<div class="box-element product">
-				<h6><strong>Product</strong></h6>
-				<hr>
-				<center><a class="btn btn-outline-success" href="#"><button><i class="fa fa-download"></i> Download</button></a></center>
-				
 
-			</div>
-		</div>
 
-		<div class="col-lg-2">
-        <img class="thumbnail" src="images/placeholder.png">
-			<div class="box-element product">
-				<h6><strong>Product</strong></h6>
-				<hr>
-				
-				<center><a class="btn btn-outline-success" href="#"><button><i class="fa fa-download"></i> Download</button></a></center>
-				
-				
-			</div>
-		</div>
 
 
-	
 
 
-		<div class="col-lg-2">
-        <img class="thumbnail" src="images/placeholder.png">
-			<div class="box-element product">
-				<h6><strong>Product</strong></h6>
-				<hr>
-				
-				<center><a class="btn btn-outline-success" href="#"><button><i class="fa fa-download"></i> Download</button></a></center>
-				
-				
-			</div>
-		</div>
 
 
-		<div class="col-lg-2">
-			<img class="thumbnail" src="images/placeholder.png">
-				<div class="box-element product">
-					<h6><strong>Product</strong></h6>
-					<hr>
-					
-					<center><a class="btn btn-outline-success" href="#"><button><i class="fa fa-download"></i> Download</button></a></center>
-					
-					
-				</div>
-			</div>
 
 
-			<div class="col-lg-2">
-				<img class="thumbnail" src="images/placeholder.png">
-					<div class="box-element product">
-						<h6><strong>Product</strong></h6>
-						<hr>
-						
-						<center><a class="btn btn-outline-success" href="#"><button><i class="fa fa-download"></i> Download</button></a></center>
-						
-						
-					</div>
-				</div>
 
 
-        <div class="col-lg-2">
-        <img class="thumbnail" src="images/placeholder.png">
-			<div class="box-element product">
-				<h6><strong>Product</strong></h6>
-				<hr>
-				
-				<center><a class="btn btn-outline-success" href="#"><button><i class="fa fa-download"></i> Download</button></a></center>
-				
-				
-				
-			</div>
-		</div>
 
-	
-	</div>
 
+    <!--header section ends-->
+   
 
+<!--login form container-->
 
 
 
@@ -221,23 +57,103 @@
 
 
 
+<!--home section start-->
 
+<section class="home" id="home">
 
+    <div class="content">
+        <h3>adventure is worthwhile</h3>
+        <p>discover new places with us, adventure awaits</p>
+        <a href="#" class="btn">discover more</a>
+    </div>
 
+<span class="vid-btn" data-src="image/4.mp"></span>
 
+    <div class="controls">
+        
+    </div>
+    <div class="video-container">
+        
+        <video src="image/4.mp4" id="video-slider" loop autoplay muted></video>
+       
+    </div>
+    
+    
+    
+    </section>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  
 
+     
+    
+          <!--home section ended-->
+    
 
 
+<!--footer section starts-->
 
 
+<section class="footer">
+    <div class="box-container">
 
 
+        <div class="box">
+            <h3>about us</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Error repudiandae nihil nisi ut aspernatur! Quas fuga 
+                sequi, quidem eius natus dignissimos cumque. 
+                
+              
+            </p>
+        </div>
 
 
+        <div class="box">
+            <h3>branch location</h3>
+            <a href="#">assosa</a>
+            <a href="#">nekemt</a>
+            <a href="#">adama</a>
+            <a href="#">ambo</a>
+        </div>
 
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="index.php">home</a>
+            <a href="book.php">book</a>
+            <a href="packages.php">packages</a>
+            <a href="gallery.php">gallery</a>
+            <a href="services.php">services</a>
+            <a href="review.php">review</a>
+            <a href="contact.php">contact</a>
+        </div>
 
 
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="https://www.facebook.com/dawit.tcool">facebook</a>
+            <a href="https://www.instagram.com/dawit_1313/">instagram</a>
+            <a href="https://github.com/dawittamene">GitHub</a>
+       
+            <a href="https://twitter.com/dawittamene559">twitter</a>
+            <a href="https://www.linkedin.com/in/dawit-tamene-531194273/">linledin</a>
+        </div>
 
+    </div>
+    <h1 class="credit"> created by <span> zumbara web developer group </span> | all rights reserved! </h1>
+</section>
+<!--footer section ends-->
 
 
 
@@ -251,30 +167,7 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <!--coustom js file link-->
+    <script src="index.js"></script>
 </body>
 </html>
